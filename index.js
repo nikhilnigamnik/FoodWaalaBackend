aaa7ecf9cb2f15c5486e065131cdfc81c0f9535a
Nikhil Nigam <73949924+nikhilnigamnik@users.noreply.github.com>
2023-08-28 00:11:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -215,6 +215,16 @@ app.get("/product", async (req, res) => {
   res.send(JSON.stringify(data));
 });
 
+// get user 
+app.get("/users", async (req, res) => {
+  try {
+    const users = await userModel.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // Stripe Payment
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
